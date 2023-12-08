08fe2265764187ca6cc4b99a77ce68afda9ec297
Aurimas Gervelis <agervelis@reiz.tech>
2023-12-08 15:40:56 +0200

    Add Jest tests2

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
-function sum(a, b) {
-    return a + b;
-  }
-  
-  function subtract(a, b) {
-    return a - b;
-  }
-  
-  function divide(a, b) {
-    return a / b;
-  }
-  
-  function multiply(a, b) {
-    return a * b;
-  }
-  
-  module.exports = { sum, subtract, divide, multiply };
-  
+// test.js
+
+const { test, expect } = require('@jest/globals');
+
+const { sum, subtract, divide, multiply } = require('./your-math-functions'); // Replace with the actual file path
+
+test('adds 1 + 3 to equal 4', () => {
+  expect(sum(1, 3)).toBe(4);
+});
+
+test('subtracts 1 - 3 to equal -2', () => {
+  expect(subtract(1, 3)).toBe(-2);
+});
+
+test('divides 1 / 3 to equal 0.333...', () => {
+  expect(divide(1, 3)).toBeCloseTo(0.333, 3);
+});
+
+test('multiplies 1 * 3 to equal 3', () => {
+  expect(multiply(1, 3)).toBe(3);
+});

5398997f0a4b2dfd7e9e49bd67956dc489d71541
Aurimas Gervelis <agervelis@reiz.tech>
2023-12-08 15:45:33 +0200

    Add Jest tests4

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 const { test, expect } = require('@jest/globals');
 
-const { sum, subtract, divide, multiply } = require('./your-math-functions'); // Replace with the actual file path
+const { sum, subtract, divide, multiply } = require('./test2');
 
 test('adds 1 + 3 to equal 4', () => {
   expect(sum(1, 3)).toBe(4);
